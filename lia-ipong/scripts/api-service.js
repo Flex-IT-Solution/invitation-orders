b64f44b495a38edc1ba751d7c33d6b91029f4a11
akuBetaa <beta.n.awwalin@gmail.com>
2025-05-30 12:55:01 +0700

    update id

--- lia-ipong/scripts/api-service.js
+++ lia-ipong/scripts/api-service.js
@@ -12,8 +12,8 @@ if (namaTamu) {
   document.getElementById("nama-tamu").textContent = `${namaTamu}`;
 }
 
-const urlBase = "https://api.flexation.id/api/v0";
-const undanganId = 11;
+const urlBase = "http://43.165.194.10:30080/api/v0";
+const undanganId = 4;
 localStorage.setItem("undanganId", undanganId);
 
 // Mengambil Token
