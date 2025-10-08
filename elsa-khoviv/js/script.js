2662ffb445f639b80966429be50d535f0843cfe8
akuBetaa <beta.n.awwalin@gmail.com>
2025-10-08 22:05:34 +0700

    fixing

--- elsa-khoviv/js/script.js
+++ elsa-khoviv/js/script.js
@@ -1,3 +1,17 @@
+// Fungsi untuk mengambil nilai parameter dari URL
+function getQueryParameter(param) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+
+// Ambil nama tamu dari URL
+const namaTamu = getQueryParameter("tamu");
+
+// Tampilkan nama tamu pada bagian cover, jika ada
+if (namaTamu) {
+  document.getElementById("nama-tamu").textContent = `${namaTamu}`;
+}
+
 // Cover 
 function closeCover() {
   const coverModal = document.getElementById('cover-modal');
