504ea457cd1d0013866f36873146bf1d6772c583
akuBetaa <beta.n.awwalin@gmail.com>
2025-10-29 09:13:01 +0700

    create new template

---
+++ vivi-rohadi/3/src/scripts/index.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+      },
+      fontFamily: {
+        cinzel: ['"Cinzel Decorative"', 'serif'],
+        jakarta: ['"Plus Jakarta Sans"', 'sans-serif'],
+        praise: ['"Praise"', 'cursive'],
+      },
+    },
+  },
+};
